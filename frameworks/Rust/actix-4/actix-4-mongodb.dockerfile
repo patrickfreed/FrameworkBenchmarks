456b8f55fe73d50ef39fa28039843c866de9e82b
Patrick Freed <patrick.freed@mongodb.com>
2022-02-01 18:02:15 -0500

    Implement actix-web 4.0 benchmarks (#1)

---
+++ frameworks/Rust/actix-4/actix-4-mongodb.dockerfile
@@ -0,0 +1,15 @@
+FROM rust:1.57.0
+
+ENV ACTIX_TECHEMPOWER_MONGODB_URL=mongodb://tfb-database:27017
+
+RUN apt-get update -yqq && apt-get install -yqq cmake g++
+
+ADD ./ /actix-4
+WORKDIR /actix-4
+
+RUN cargo clean
+RUN RUSTFLAGS="-C target-cpu=native" cargo build --release --bin actix-4-mongodb
+
+EXPOSE 8080
+
+CMD ./target/release/actix-4-mongodb
---
+++ frameworks/Rust/actix-4/src/main.rs
@@ -0,0 +1,75 @@
+use std::io;
+
+use actix_http::KeepAlive;
+use actix_web::{
+    http::{
+        header::{HeaderValue, CONTENT_TYPE, SERVER},
+        StatusCode,
+    },
+    web::{self, BufMut, Bytes, BytesMut},
+    App, HttpResponse, HttpServer,
+};
+use anyhow::Result;
+use simd_json_derive::Serialize;
+
+pub const SIZE: usize = 27;
+
+pub struct Writer<'a>(pub &'a mut BytesMut);
+
+impl<'a> io::Write for Writer<'a> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.put_slice(buf);
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Serialize)]
+pub struct Message {
+    pub message: &'static str,
+}
+
+async fn json() -> HttpResponse<Bytes> {
+    let message = Message {
+        message: "Hello, World!",
+    };
+    let mut body = BytesMut::with_capacity(SIZE);
+    message.json_write(&mut Writer(&mut body)).unwrap();
+
+    let mut res = HttpResponse::with_body(StatusCode::OK, body.freeze());
+    res.headers_mut()
+        .insert(SERVER, HeaderValue::from_static("A"));
+    res.headers_mut()
+        .insert(CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    res
+}
+
+async fn plaintext() -> HttpResponse<Bytes> {
+    let mut res = HttpResponse::with_body(StatusCode::OK, Bytes::from_static(b"Hello, World!"));
+    res.headers_mut()
+        .insert(SERVER, HeaderValue::from_static("A"));
+    res.headers_mut()
+        .insert(CONTENT_TYPE, HeaderValue::from_static("text/plain"));
+    res
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    println!("Started http server: 127.0.0.1:8080");
+
+    HttpServer::new(move || {
+        App::new()
+            .service(web::resource("/json").to(json))
+            .service(web::resource("/plaintext").to(plaintext))
+    })
+    .keep_alive(KeepAlive::Os)
+    .client_timeout(0)
+    .backlog(1024)
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await?;
+
+    Ok(())
+}
